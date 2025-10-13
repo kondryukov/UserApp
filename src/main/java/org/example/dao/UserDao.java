1a425098a81cc3e856da0f7b48157a88b14a3ff3
kondryukov <80988635+kondryukov@users.noreply.github.com>
2025-10-13 12:52:04 +0300

    Update UserDao.java
    Change return value of update

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -5,7 +5,7 @@ import org.example.domain.User;
 public interface UserDao {
     void create(User user);
     User read(Long id);
-    void update(User user);
+    User update(User user);
     void deleteById(Long id);
     void deleteByEmail(String email);
     boolean mailUniqueCheck(String email);

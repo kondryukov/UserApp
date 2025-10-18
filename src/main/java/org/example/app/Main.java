fe76de5d97ece0b39972f01568de0c7d32d6bac7
mark <kondryokov@gmail.com>
2025-10-18 18:14:20 +0300

    some fix

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         SessionFactory sf = null;
         String command;
-
+        System.out.println("hello");
         try {
             sf = HibernateUtil.getSessionFactory();
             UserService userService = createUserService(sf);
